4ec2c6b41f8621e5c18e15460ce05d3288b8ac8f
Greg Knapp <greg.knapp@eckoh.com>
2014-05-19 18:09:11 +0100

    Sprite image not rendering on canvas

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject jump "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject jump "0.1.0"
+  :description "A platform game"
   :url "http://github.com/gknapp/jump"
   :source-paths ["src/clj" "src/cljs"]
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -9,5 +9,7 @@
               [{:source-paths ["src/cljs"]
                 :compiler {:output-to "resources/public/js/jump/main.js"
                            :output-dir "resources/public/js/jump"
+                           :source-map "resources/public/js/jump/jump.js.map"
+                           ; :source-map-path "js/jump"
                            :optimizations :whitespace
                            :pretty-print true}}]})
--- src/cljs/jump/core.cljs
+++ src/cljs/jump/core.cljs
@@ -1,3 +1,44 @@
-(ns jump-cljs.core)
+(ns jump.core)
 
-(.write js/document "Hello World!")
+(enable-console-print!)
+
+(defn screen
+  ([]
+     (screen "screen"))
+  ([elem-id]
+     (let [screen (.getElementById js/document elem-id)]
+       {:context (.getContext screen "2d")
+        :height (.-height screen)
+        :width (.-width screen)})))
+
+(defn draw-sprite
+  [player screen]
+  (let [ctx (:context screen)
+        sprite (:image player)
+        w (:width player)
+        h (:height player)
+        o-top 94
+        o-left 167
+        o-height (+ o-top h)
+        o-width (+ o-left w)]
+    (println o-top o-left w h)
+    (.drawImage ctx sprite 10 10)))
+
+(defn image
+  [src]
+  (let [img (new js/Image)]
+    (set! (.-src img) src)
+    img))
+
+(def player
+  {:image (image "sprites/player.png")
+   :height 35
+   :width 37
+   :facing :right
+   :falling false
+   :jumping false})
+
+(set! (.-onload js/window)
+      (do
+        (println "Window loaded")
+        (draw-sprite player (screen))))
