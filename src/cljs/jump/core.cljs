6f7d1323e029896c1f1b995837705ab2ad564cfb
Greg Knapp <greg.knapp@eckoh.com>
2014-05-20 18:32:18 +0100

    Shelf sprites, establish component entity system

--- src/cljs/jump/core.cljs
+++ src/cljs/jump/core.cljs
@@ -1,46 +1,27 @@
-(ns jump.core)
+(ns jump.core
+  (:require [jump.components :as component]
+   [jump.ui :refer [request-frame render]]))
 
 (enable-console-print!)
 
-(defn screen
-  ([]
-     (screen "screen"))
-  ([elem-id]
-     (let [screen (.getElementById js/document elem-id)]
-       {:context (.getContext screen "2d")
-        :height (.-height screen)
-        :width (.-width screen)})))
+(def game
+  {:entities
+   [{:id :player
+     :traits [(component/position 50 565)
+              (component/walk 4 :right)
+              (component/jump 60)
+              (component/gravity 10)
+              (component/control :keyboard)
+              (component/mortal)
+              (component/solid)
+              (component/renderable 37 35)]}]})
 
-(defn draw-sprite
-  [player screen]
-  (let [ctx (:context screen)
-        sprite (:image player)
-        t (:top player)
-        l (:left player)
-        w (:width player)
-        h (:height player)]
-    (println ctx sprite l t w h 10 10 w h)
-    (.drawImage ctx sprite l t w h 10 10 w h)))
+(defn update []
+  game)
 
-(defn image
-  [src]
-  (let [img (new js/Image)]
-    (set! (.-src img) src)
-    img))
+(defn animate []
+  (-> update
+      render)
+  (request-frame animate))
 
-(def player
-  {:image (image "sprites/player.png")
-   :top 94
-   :left 167
-   :height 35
-   :width 37
-   :facing :right
-   :falling false
-   :jumping false})
-
-(set! (.-onload js/window)
-      (do
-        (println "Window loaded")
-        (println "player" player)
-        (println "screen" (screen))
-        (draw-sprite player (screen))))
+(set! (.-onload js/window) animate)
