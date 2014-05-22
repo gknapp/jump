8fdd56ddd8f57bfe6183a7bf62d4d0fe5595a0ed
Greg Knapp <greg.knapp@eckoh.com>
2014-05-22 17:18:45 +0100

    Implement walk

--- src/cljs/jump/system/physics.cljs
+++ src/cljs/jump/system/physics.cljs
@@ -9,8 +9,10 @@
   [entity]
   (let [has-position (ent/trait :position entity)
         takes-input (ent/trait :input entity)
+        can-jump (ent/trait :jump entity)
         can-walk (ent/trait :walk entity)]
-    (and has-position takes-input can-walk)))
+    (and has-position takes-input
+         (or can-jump can-walk))))
 
 (defn falls?
   [entity]
@@ -21,12 +23,17 @@
 
 ;; Actions
 
-(defn walk [entities cmd]
-  (doseq [entity entities]
-    (let [{:keys [x step]} (ent/trait :position entity)
-          mov (if (= cmd :left) - +)]
-      ; needs to effect game
-      (mov x step))))
+(defn walk? [cmd]
+  (#{:left :right} cmd))
+
+(defn walk [entity cmd]
+  (let [{:keys [x]} (ent/trait :position entity)
+        {:keys [step facing]} (ent/trait :walk entity)
+        op (if (= cmd :left) - +)
+        update (-> entity
+                    (assoc-in [:traits :position :x] (op x step))
+                    (assoc-in [:traits :walk :facing] cmd))]
+    (merge entity update)))
 
 (defn jump [entities]
   {})
@@ -34,15 +41,21 @@
 (defn fall [entities]
   {})
 
+(defn merge-updates
+  [game updates]
+  (let [ids-match? #(= (:id %1) (:id %2))]
+    (vec (for [e game u updates :when (ids-match? e u)]
+           {:id (:id e)
+            :traits (merge (:traits e) (:traits u))}))))
 
-(defn move [commands game]
-  (go
-    (let [entities (filter moveable? @game)
-          cmd (<! commands)]
-      (cond
-       (#{:left :right} cmd) (walk entities cmd))
-       ;swap! entities into game
-      )))
+(defn move [input game]
+  (when-let [moveables (filter moveable? game)]
+    (go
+      (let [cmd (<! input)
+            updates (cond
+                     (walk? cmd) (map #(walk % cmd) moveables)
+                     (= :jump cmd) moveables)]
+        (merge-updates game updates)))))
 
 (defn gravity [game]
   (let [entities (filter falls? game)]
